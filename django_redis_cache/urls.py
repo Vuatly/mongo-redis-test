05308c7b443c72104ebd48a0770c4b17ea826052
Vuatly <53141022+Vuatly@users.noreply.github.com>
2020-03-23 19:34:50 +0300

    Update urls.py
    fix

--- django_redis_cache/urls.py
+++ django_redis_cache/urls.py
@@ -22,9 +22,3 @@ urlpatterns = [
     path('', include('board.urls'))
 ]
 
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
